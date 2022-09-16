c296d654e79fa813207ef9a5e99f3c444ff7560e
honsumal <14leealastair@gmail.com>
2022-09-15 22:57:08 -0400

    finished draft

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# module-3-challenge
+# Password Generator
+
+## Description
+
+This application generates a random password of length between 8 and 128 characters. The user is prompted on whether or not they wish to have lowercase, uppercase, numeric, or special characters in their password. The randomization is generated using the crypto.getRandomValues method, as it is provides cryptographically secure random numbers, as indicated by the MDN documentation on Math.random(). Below, I have outlined the various functions written that provide the application's functionality.
+
+### writePassword
+
+This function takes the password generated from generatePassword (seen below), and casts it onto a box after it is generated.
+
+### rDec
+This function creates a random number between 0 and 1 by using the crypto.getRandomValues method. Dividing by the length of the Character Set allows for the function to generate an even spread of decimals across the span of the character list.
+
+### generatePassword
+This function contains both the charSelect and makePassword functions. The first part of the function asks the user to input the desired length of their password, with redundancy built in to ensure a valid input is recieved.
+
+### charSelect
+This function is nestled within the generatePassword function, and asks the user to select the types of characters they want to use in their password. If the user selects no character types, they are forced to remake their choices.
+
+### makePassword
+This function is nestled within the generatePassword function, and its main purpose is to take the information from the user prompts and convert that into a password of the user's specifications.
+
+## Installation
+
+This application runs from the browser and thus needs no installation.
+
+## Usage
+The site is fully operational, with all links tested and images backed up with alternate text in the case of potential problems. Link to deployed webpage: https://honsumal.github.io/module-3-challenge/
+
+A screenshot of the completed application is provided below:
+
+![Completed Webpage Image](/assets/images/finished-webpage.png)
+
+## Credits
+
+N/A
+
+## License
+
+N/A
+
--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -12,3 +12,80 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Using crypto.getRandomValues to get a more secure random value.
+function rDec (charSetLength) {
+  let array = new Uint32Array(1);
+  self.crypto.getRandomValues(array);
+
+//The following operations ensure that we get an even distribution of values for each character in the usable set.
+  val = array[0]/(charSetLength) 
+  rDecimal = val - Math.floor(val)
+  return rDecimal
+}
+
+//Prompts
+function generatePassword(){
+  let length = prompt("Please enter your desired length of password between 8 and 128 characters")
+  length = Math.floor(length) //Converts input into a number, and also acts as a redundancy in case user inputs a non-integer
+
+// Adds a redundancy in case user accidentally inputs in something other than a number or a number that is out of the scope of the password generator
+  while (length < 8 || length > 128 || isNaN(length)){
+    length = prompt("Sorry, your input was invalid, please try again")
+  }
+  
+  function charSelect () {
+    let lConf = false;
+    let uConf = false;
+    let nConf = false;
+    let sConf = false;
+
+//Adds a redundancy in case a user selects no character types to be used in their password
+    while (!lConf && !uConf && !nConf && !sConf) {
+      lConf = confirm("Would you like lowercase letters in your password");
+      uConf = confirm("Would you like uppercase letters in your password");
+      nConf = confirm("Would you like numerals in your password");
+      sConf = confirm("Would you like special characters in your password");
+
+      if (!lConf && !uConf && !nConf && !sConf){
+        alert("Please add at least one character type.")
+      }
+    }
+    return [lConf, uConf, nConf, sConf]
+  }
+
+  Conf = charSelect();
+
+//Makes Pasword with the above prompts
+  function makePassword (length, lConf, uConf, nConf, sConf){
+    let lower = "abcdefghijklmnopqrstuvwxyz";
+    let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let number = "0123456789";
+    let spec = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    let charSet = ""
+
+//Creates Character list
+    let conf = [lConf, uConf, nConf, sConf]
+    let allChars = [lower, upper, number, spec]
+
+    for (x in conf){
+      if (conf[x]){
+        charSet += allChars[x];
+      }
+    }
+
+    newPass = ""; //Defines the new password as an empty string to add the new characters in
+
+    for (i = 0, n = charSet.length; i < length; i ++){
+      newPass += charSet.charAt(Math.floor(rDec(n)* n)); //Generates password using rDec function from line 17-24
+    }
+
+    return newPass
+  }
+
+  return makePassword(length, Conf[0], Conf[1], Conf[2], Conf[3])
+}
+
+
+
+
